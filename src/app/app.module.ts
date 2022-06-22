ab550344803b1940cd0c974a5e058666fa32f5eb
Cristiane Guimarães Fröhlich <cristiane_g_frohlich@hotmail.com>
2022-06-22 19:53:54 -0300

    dashboard
    Dashboard criada com Angular, com integração da API de gráficos do Google

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    DashboardModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
